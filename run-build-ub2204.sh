a3c5ec448106b3f4ecc1f689d93b63480189b8da
Kenneth Davidson <xylene2016@protonmail.com>
2025-08-06 04:20:09 -0400

    Update run-build-ub2204.sh

--- run-build-ub2204.sh
+++ run-build-ub2204.sh
@@ -30,6 +30,31 @@ sleep 2
 /bin/systemctl stop docker.socket docker.service containerd.service
 /bin/rm -fr /var/lib/docker/* /var/lib/containerd/* /mnt/docker-data/*
 
+# OpenSSL 3.5
+systemctl start docker
+sleep 2
+if [ "$(cat /proc/cpuinfo | grep -i '^processor' | wc -l)" -gt 1 ]; then
+    docker run --cpus="$(cat /proc/cpuinfo | grep -i '^processor' | wc -l).0" --rm --name ub2204 -itd ubuntu:22.04 bash
+else
+    docker run --rm --name ub2204 -itd ubuntu:22.04 bash
+fi
+sleep 2
+docker exec ub2204 apt update -y
+#docker exec ub2204 apt upgrade -fy
+docker exec ub2204 apt install -y bash vim wget ca-certificates curl
+docker exec ub2204 /bin/ln -svf bash /bin/sh
+docker exec ub2204 /bin/bash -c '/bin/rm -fr /tmp/*'
+docker cp ub ub2204:/home/
+docker exec ub2204 /bin/bash /home/ub/2204/.preinstall_ub2204
+docker exec ub2204 /bin/bash /home/ub/build-openssl-libs-ub.sh '3.5'
+mkdir -p /tmp/_output_assets
+docker cp ub2204:/tmp/_output /tmp/_output_assets/
+sleep 2
+/bin/systemctl stop docker.socket docker.service containerd.service
+/bin/rm -fr /var/lib/docker/* /var/lib/containerd/* /mnt/docker-data/*
+
+exit
+
 # OpenSSL 3.3
 systemctl start docker
 sleep 2
@@ -52,29 +77,3 @@ docker cp ub2204:/tmp/_output /tmp/_output_assets/
 sleep 2
 /bin/systemctl stop docker.socket docker.service containerd.service
 /bin/rm -fr /var/lib/docker/* /var/lib/containerd/* /mnt/docker-data/*
-
-# OpenSSL 3.4
-systemctl start docker
-sleep 2
-if [ "$(cat /proc/cpuinfo | grep -i '^processor' | wc -l)" -gt 1 ]; then
-    docker run --cpus="$(cat /proc/cpuinfo | grep -i '^processor' | wc -l).0" --rm --name ub2204 -itd ubuntu:22.04 bash
-else
-    docker run --rm --name ub2204 -itd ubuntu:22.04 bash
-fi
-sleep 2
-docker exec ub2204 apt update -y
-#docker exec ub2204 apt upgrade -fy
-docker exec ub2204 apt install -y bash vim wget ca-certificates curl
-docker exec ub2204 /bin/ln -svf bash /bin/sh
-docker exec ub2204 /bin/bash -c '/bin/rm -fr /tmp/*'
-docker cp ub ub2204:/home/
-docker exec ub2204 /bin/bash /home/ub/2204/.preinstall_ub2204
-docker exec ub2204 /bin/bash /home/ub/build-openssl-libs-ub.sh '3.4'
-mkdir -p /tmp/_output_assets
-docker cp ub2204:/tmp/_output /tmp/_output_assets/
-sleep 2
-/bin/systemctl stop docker.socket docker.service containerd.service
-/bin/rm -fr /var/lib/docker/* /var/lib/containerd/* /mnt/docker-data/*
-
-exit
-
